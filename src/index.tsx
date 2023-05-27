ed0d9d98411a7899996cba97b33125f0497ab585
rashad.murad023@gmail.com <moriarty@Moriartys-MacBook-Pro.local>
2023-05-28 01:28:18 +0400

    vercel test 2

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
-//testing vercel
+//testing vercel2
 root.render(
   <React.StrictMode>
     <App />
